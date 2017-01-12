0bb62d9bdb69efd17b73a3c501e1f23fad21031e
s152144 <cornelius.a.r@gmail.com>
2017-01-12 11:42:39 +0100

    switched to pointers and updated the struct

--- poisson.c
+++ poisson.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct tuple {
-  double v0;
-  double v1;
+struct twoPointers {
+  double** pointers[2];
   int using;
 };
 
-void printMatrix(int N, double matrix[N][N]) {
+void printMatrix(int N, double** matrix) {
   int r, c;
   for(c = 0; c < N; c++) {
     for(r = 0; r < N; r++) {
@@ -16,7 +15,7 @@ void printMatrix(int N, double matrix[N][N]) {
   }
 }
 
-void writeMatrix(int N, double matrix[N][N]) {
+void writeMatrix(int N, struct twoPointers mydata) {
   FILE *fpout;
   fpout = fopen("data.dat", "wb+");
     if (fpout == NULL) {
@@ -30,22 +29,23 @@ void writeMatrix(int N, double matrix[N][N]) {
     for(r = 0; r < N; r++) {
       double x = r * 2.0/((double) (N-1)) - 1;
       double y = c * 2.0/((double) (N-1)) - 1;
-      fprintf(fpout, "%lf %lf %lf\n", x, y, matrix[r][c]);
+      fprintf(fpout, "%lf %lf %lf\n", x, y, mydata.pointers[mydata.using][r][c]);
     }
   }
   fclose(fpout);
 }
 
-void jacobi(int N, int loops, double matrix[N][N]) {
+void jacobi(int N, int loops, struct twoPointers mydata) {
   double spacing = 2.0/((double) N);
   printf("spacing=%lf\n", spacing);
 
   //printMatrix(N, matrix);
-  writeMatrix(N, matrix);
+  writeMatrix(N, mydata);
 }
 
 int main(int argc, char *argv[]) {
   int N = 3;
+  int i;
   if(argc >= 2) { N = atoi(argv[1]); }
   printf("N=%i\n", N);
 
@@ -53,7 +53,38 @@ int main(int argc, char *argv[]) {
   if(argc >= 3) { loops = atoi(argv[2]); }
   printf("loops=%i\n", loops);
 
-  double matrix[N][N];
+  struct twoPointers mydata;
+  mydata.using = 0;
+  double **matrix;
+
+  if ( (matrix = calloc( N, sizeof(double*) )) == NULL ) {
+	perror("main(__LINE__), allocation failed");
+	exit(1);
+  }
+  
+  for(i = 0; i < N; i++) {
+    if ( (matrix[i] = calloc( N, sizeof(double) )) == NULL ) {
+	perror("main(__LINE__), allocation failed");
+	exit(1);
+    }
+  }
+
+  double **matrix2;
+
+  if ( (matrix2 = calloc( N, sizeof(double*) )) == NULL ) {
+	perror("main(__LINE__), allocation failed");
+	exit(1);
+  } 
+  for(i = 0; i < N; i++) {
+    if ( (matrix2[i] = calloc( N, sizeof(double) )) == NULL ) {
+	perror("main(__LINE__), allocation failed");
+	exit(1);
+    }
+  }
+
+  mydata.pointers[0] = matrix;
+  mydata.pointers[1] = matrix2;
+  mydata.using = 0;
 
   //Initialization Outer
   int r,c;
@@ -74,6 +105,13 @@ int main(int argc, char *argv[]) {
       matrix[r][c] = initialGuess;
     }
   }
-  jacobi(N, loops, matrix);
+  printf("main: running jacobi\n");
+  jacobi(N, loops, mydata);
+  for(i = 0; i < N; i++) {
+    free(matrix[i]);
+    free(matrix2[i]);
+  }
+  free(matrix);
+  free(matrix2);
   return 0;
 }
